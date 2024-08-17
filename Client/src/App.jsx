8c3767f4cd9c6cdd26b98f67521b8e2e415c79a8
Trisha-tech <1234trishasahu@gmail.com>
2024-08-17 23:13:17 +0530

    first commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AuroraHero } from "./components/AuroraHero";
+
+const App = () => {
+  return (
+    <>
+      <AuroraHero />
+    </>
+  );
+};
+
+export default App;
